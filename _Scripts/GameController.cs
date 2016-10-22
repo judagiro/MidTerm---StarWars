410597fdafa8d794585364c4ce5ac3d0d35021bf
jgirald2 <300914896@cencol.ca>
2016-10-22 15:41:59 -0400

    Added tasks 2 and 3

--- _Scripts/GameController.cs
+++ _Scripts/GameController.cs
@@ -1,14 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using UnityEngine.UI;
+
 public class GameController : MonoBehaviour {
 	// PUBLIC INSTANCE VARIABLES
 	public int enemyCount;
 	public GameObject enemy;
-	
-	// Use this for initialization
-	void Start () {
-		this._GenerateEnemies ();
+    private int _hullValue;
+    private int _scoreValue;
+
+    [Header("UI Objects")]
+    public Text HullLabel;
+    public Text ScoreLabel;
+
+
+    
+
+    public int HullValue
+    {
+        get
+        {
+            return this._hullValue;
+        }
+
+        set
+        {
+            this._hullValue = value;
+            if (this._hullValue <= 0)
+            {
+                this._endGame();
+            }
+            else
+            {
+                this.HullLabel.text = "Hull: " + this._hullValue;
+            }
+        }
+    }
+
+    public int ScoreValue
+    {
+        get
+        {
+            return this._scoreValue;
+        }
+
+        set
+        {
+            this._scoreValue = value;
+            this.ScoreLabel.text = "Score: " + this._scoreValue;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        this.HullValue = 5;
+        this.ScoreValue = 0;
+        this._GenerateEnemies ();
+
 	}
 	
 	// Update is called once per frame
@@ -22,4 +71,16 @@ public class GameController : MonoBehaviour {
 			Instantiate(enemy);
 		}
 	}
+
+    private void _endGame()
+    {
+        //this.GameOverLabel.gameObject.SetActive(true);
+        this.ScoreLabel.text = "Final Score: " + this.ScoreValue;
+        this.ScoreLabel.gameObject.SetActive(true);
+        //this.RestartButton.gameObject.SetActive(true);
+        this.ScoreLabel.gameObject.SetActive(false);
+        this.HullLabel.gameObject.SetActive(false);
+        
+        
+    }
 }
--- _Scripts/PlayerController.cs
+++ _Scripts/PlayerController.cs
@@ -1,30 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-	// PUBLIC INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++++
-	//public float speed;
-	public Boundary boundary;
+public class PlayerController : MonoBehaviour
+{
+    // PUBLIC INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++++
+    //public float speed;
+    public Boundary boundary;
 
-	public Camera camera;
-	
-	// PRIVATE INSTANCE VARIABLES
-	private Vector2 _newPosition = new Vector2(0.0f, 0.0f);
-	
-	// Use this for initialization
-	void Start () {
+    public Camera camera;
 
-	}
+    public GameController gameController;
 
-	// Update is called once per frame
-	void Update () {
-		this._CheckInput ();
-	}
+    private Transform _transform;
+    private Rigidbody2D _rigidbody;
 
-	private void _CheckInput() {
-		this._newPosition = gameObject.GetComponent<Transform> ().position; // current position
+    
+    
 
-		/* movement by keyboard
+
+    // PRIVATE INSTANCE VARIABLES
+    private Vector2 _newPosition = new Vector2(0.0f, 0.0f);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._CheckInput();
+    }
+
+    private void _CheckInput()
+    {
+        this._newPosition = gameObject.GetComponent<Transform>().position; // current position
+
+        /* movement by keyboard
 		if (Input.GetAxis ("Horizontal") > 0) {
 			this._newPosition.x += this.speed; // add move value to current position
 		}
@@ -35,22 +48,36 @@ public class PlayerController : MonoBehaviour {
 		}
 		*/
 
-		// movement by mouse
-		Vector2 mousePosition = Input.mousePosition;
-		this._newPosition.x = this.camera.ScreenToWorldPoint (mousePosition).x;
+        // movement by mouse
+        Vector2 mousePosition = Input.mousePosition;
+        this._newPosition.x = this.camera.ScreenToWorldPoint(mousePosition).x;
 
-		this._BoundaryCheck ();
+        this._BoundaryCheck();
 
-		gameObject.GetComponent<Transform>().position = this._newPosition;
-	}
+        gameObject.GetComponent<Transform>().position = this._newPosition;
+    }
 
-	private void _BoundaryCheck() {
-		if (this._newPosition.x < this.boundary.xMin) {
-			this._newPosition.x = this.boundary.xMin;
-		}
+    private void _BoundaryCheck()
+    {
+        if (this._newPosition.x < this.boundary.xMin)
+        {
+            this._newPosition.x = this.boundary.xMin;
+        }
 
-		if (this._newPosition.x > this.boundary.xMax) {
-			this._newPosition.x = this.boundary.xMax;
-		}
-	}
+        if (this._newPosition.x > this.boundary.xMax)
+        {
+            this._newPosition.x = this.boundary.xMax;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+
+            this.gameController.HullValue -= 1;
+        }
+        
+    }
 }
